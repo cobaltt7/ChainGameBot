534fcfd5f243616b43e30636382a9d020b39b838
RedGuy12 <paul@reid-family.org>
2021-03-23 14:11:04 -0500

    stuffs

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-require("dotenv").config();
+(async function () {
+	require("dotenv").config();
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const SQL = require("pg").Client;
-const Database = new SQL({
-	user: process.env.DATABASE_USER,
-	password: process.env.DATABSE_PASSWORD,
-	host: process.env.DATABASE_HOST,
-	port: process.env.DATABASE_PORT,
-	database: process.env.DATABASE_NAME,
-});
-Database.connect()
-	.then(console.log("Connected to Postgres"))
-	.catch(console.error);
-client.on("ready", () => {
-	console.log(`Connected to Discord`);
-});
-
-client.on("message", (msg) => {
-	const channel = client.channels.cache.get("823941849453821982");
-	channel.messages.fetch({ limit: 2 }).then((messages) => {
-		console.log(messages);
-		if (
-			messages[1].content[
-				messages[1].content.length - 1
-			].toLowerCase() !== msg.content[0].toLowerCase()
-		) {
-			client.channels.cache.get("823941821695918121").send("Pong");
-		}
+	const SQL = require("pg").Client;
+	const Database = new SQL({
+		user: process.env.DATABASE_USER,
+		password: process.env.DATABSE_PASSWORD,
+		host: process.env.DATABASE_HOST,
+		port: process.env.DATABASE_PORT,
+		database: process.env.DATABASE_NAME,
 	});
-});
+	await Database.connect();
+	console.log("Connected to Postgres");
+	await Database.query(
+		"CREATE TABLE IF NOT EXISTS words(\
+			word character varying(183) NOT NULL,\
+			author character varying(37) NOT NULL,\
+			id integer NOT NULL,\
+			server integer NOT NULL,\
+			channel integer NOT NULL,\
+			PRIMARY KEY (word, server, channel),\
+			UNIQUE (word)\
+		);",
+	);
 
-client.login(process.env.BOT_TOKEN);
+	const Discord = require("discord.js");
+	const client = new Discord.Client();
+	client.on("ready", () => {
+		console.log(`Connected to Discord`);
+	});
+
+	client.on("message", async (msg) => {
+		console.log(msg);
+		try {
+			await Database.query();
+		} catch {}
+	});
+
+	client.login(process.env.BOT_TOKEN);
+})();
