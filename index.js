101db8743cf88e25c9909c9ed82d4a1ce4d5014f
RedGuy12 <paul@reid-family.org>
2021-03-23 11:25:42 -0500

    Add Postgres

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ require("dotenv").config();
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
+const SQL = require("pg").Client;
+const Database = new SQL({
+	user: process.env.DATABASE_USER,
+	password: process.env.DATABSE_PASSWORD,
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	database: process.env.DATABASE_NAME,
+});
+Database.connect()
+	.then(console.log("Connected to Postgres"))
+	.catch(console.error);
 client.on("ready", () => {
-	console.log(`Logged in as ${client.user.tag}!!!!`);
+	console.log(`Connected to Discord`);
 });
 
 client.on("message", (msg) => {
 	const channel = client.channels.cache.get("823941849453821982");
 	channel.messages.fetch({ limit: 2 }).then((messages) => {
-		console.log(messages)
+		console.log(messages);
 		if (
 			messages[1].content[
 				messages[1].content.length - 1
