6396c51c016c04957656325b9059c638cf9e675b
RedGuy12 <paul@reid-family.org>
2021-03-23 14:18:41 -0500

    move to try/catch

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 (async function () {
-	require("dotenv").config();
+	try {
+		require("dotenv").config();
 
-	const SQL = require("pg").Client;
-	const Database = new SQL({
-		user: process.env.DATABASE_USER,
-		password: process.env.DATABSE_PASSWORD,
-		host: process.env.DATABASE_HOST,
-		port: process.env.DATABASE_PORT,
-		database: process.env.DATABASE_NAME,
-		ssl: true,
-	});
-	await Database.connect();
-	console.log("Connected to Postgres");
-	await Database.query(
-		"CREATE TABLE IF NOT EXISTS words(\
+		const SQL = require("pg").Client;
+		const Database = new SQL({
+			user: process.env.DATABASE_USER,
+			password: process.env.DATABSE_PASSWORD,
+			host: process.env.DATABASE_HOST,
+			port: process.env.DATABASE_PORT,
+			database: process.env.DATABASE_NAME,
+			ssl: true,
+		});
+		await Database.connect();
+		console.log("Connected to Postgres");
+		await Database.query(
+			"CREATE TABLE IF NOT EXISTS words(\
 			word character varying(183) NOT NULL,\
 			author character varying(37) NOT NULL,\
 			id integer NOT NULL,\
@@ -22,20 +23,24 @@
 			PRIMARY KEY (word, server, channel),\
 			UNIQUE (word)\
 		);",
-	);
+		);
 
-	const Discord = require("discord.js");
-	const client = new Discord.Client();
-	client.on("ready", () => {
-		console.log(`Connected to Discord`);
-	});
+		const Discord = require("discord.js");
+		const client = new Discord.Client();
+		client.on("ready", () => {
+			console.log(`Connected to Discord`);
+		});
 
-	client.on("message", async (msg) => {
-		console.log(msg);
-		try {
-			await Database.query();
-		} catch {}
-	});
+		client.on("message", async (msg) => {
+			console.log(msg);
+			return;
+			try {
+				await Database.query();
+			} catch {}
+		});
 
-	client.login(process.env.BOT_TOKEN);
+		client.login(process.env.BOT_TOKEN);
+	} catch (e) {
+		console.error(e);
+	}
 })();
