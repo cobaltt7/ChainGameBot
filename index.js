cf9a34c61e35ce275117b627a3c1b0b3511bb768
RedGuy12 <paul@reid-family.org>
2021-03-24 20:09:41 -0500

    start saving to db? pls pls workkkkk

--- index.js
+++ index.js
@@ -6,35 +6,24 @@
 			"Shirtori Discord Bot by Paul Reid // A Discord bot to enforce the rules of the Shitori game // " +
 			"https://github.com/RedGuy12/ShitoriBot";
 
-			console.log(process.env.API_ACCESS_KEY)
-		console.log(
-			await fetch({
-				url: "https://paul-s-reid.com/web-dev/ShitoriBotApi-php/index.php",
-				method: "post",
-				data: {
-					API_ACCESS_KEY: process.env.API_ACCESS_KEY,
-				},
-			}),
-		);
-		const Database = function (query, placeholders = []) {
+		const DatabaseQuery = function (query) {
 			return fetch({
 				url: "https://paul-s-reid.com/web-dev/ShitoriBotApi-php/index.php",
 				method: "post",
 				data: {
 					API_ACCESS_KEY: process.env.API_ACCESS_KEY,
 					query: query,
-					placeholders: placeholders,
 				},
 			});
 		};
+		const { escape } = require("sqlstring");
 
-		const Discord = require("discord.js");
-		const client = new Discord.Client();
-		client.on("ready", () => {
+		const Discord = new require("discord.js").Client();
+		Discord.on("ready", () => {
 			console.log(`Connected to Discord`);
 		});
 
-		client.on("message", async (msg) => {
+		Discord.on("message", async (msg) => {
 			if (msg.channel.id === "823941849453821982") {
 				const response = await fetch({
 					method: "get",
@@ -44,24 +33,27 @@
 				});
 				if (response.data.error) {
 					msg.delete();
-					client.channels.cache
+					Discord.channels.cache
 						.get("823941821695918121")
 						.send(`${msg.author} - \`${msg.content.toLowerCase()}\`` + "is not a word!");
 					return;
 				}
-				try {
-					await Database("INSERT INTO words (word, author, id, server, channel) VALUES ($1,$2,$3,$4,$5);", [
+				var used = await DatabaseQuery(
+					"SELECT `author`, `id`, `channel`, `server` FROM `shitori_words` WHERE `word`='" +
+						escape(msg.content.toLowerCase()) +
+						"'",
+				);
+				await DatabaseQuery(
+					`INSERT INTO words (word, author, id, server, channel) VALUES (${escape(
 						msg.content.toLowerCase(),
-						msg.author.toString(),
-						msg.id,
-						msg.channel.guild.id,
+					)}, ${escape(msg.author.toString())}, ${escape(msg.id)}, ${escape(msg.channel.guild.id)}, ${escape(
 						msg.channel.id,
-					]);
-				} catch {}
+					)});`,
+				);
 			}
 		});
 
-		client.login(process.env.BOT_TOKEN);
+		Discord.login(process.env.BOT_TOKEN);
 	} catch (e) {
 		console.error(e);
 	}
