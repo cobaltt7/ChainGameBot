dc9bcb7bca097eddd9ef7a1ae09afca20a24d292
RedGuy12 <paul@reid-family.org>
2021-10-12 08:21:19 -0500

    embeds

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import dotenv from "dotenv";
 import fetch from "axios";
 import mongoose from "mongoose";
-import discordJs from "discord.js";
+import {Client,Intents as intents,MessageEmbed} from "discord.js";
 
 // set up process.env
 dotenv.config();
 
 const CHANNELS = {game: "823941849453821982", rules: "823941821695918121"}
+const ME_ID = "771422735486156811"
 
 // so Wiktionary won't ban us
 fetch.defaults.headers.common["User-Agent"] =
@@ -16,7 +17,7 @@ fetch.defaults.headers.common["User-Agent"] =
 //set up db stuffs
 
 await mongoose.connect(`${process.env.MONGO_URL}?retryWrites=true&w=majority`, {
-	appName: "OneAuth",
+	appName: "bot",
 });
 mongoose.connection.on("error", console.error);
 
@@ -28,10 +29,10 @@ const Database = mongoose.model(
 
 		},
 		author: {
-			type:Number,required:true,
+			type:String,required:true,
 		},
 		id: {
-			type:Number,unique:true,required:true
+			type:String,unique:true,required:true
 		},
 		index:{
 			type:Number,required:true,unique:true
@@ -39,22 +40,32 @@ const Database = mongoose.model(
 	}),
 );
 
-const Discord = new discordJs.Client();
-Discord.on("ready", () => {
-	console.log(`Connected to Discord`);
-});
-
-Discord.on("message", async (msg) => {
-	if (msg.channel.id !== CHANNELS.game) return;
+const Discord = new Client({intents:[intents.FLAGS.GUILDS,intents.FLAGS.GUILD_MESSAGES]});
+Discord.once("ready", () =>
+	console.log(`Connected to Discord`)
+).on('debug', console.debug)
+.on('warn', console.warn)
+.on('error', console.error).on("messageCreate", async (msg) => {
+	if (msg.channelId !== CHANNELS.game) return;
 	try {
-		var word = msg.content.toLowerCase().trim();
+		var word = msg.content.toLowerCase().trim().replaceAll("`","'");
 
 		// don't allow whitespace
 		if (/\s/.test(word)) {
 			msg.delete();
+
+			const embed = new MessageEmbed()
+				.setTitle('Multiple words sent!')
+				.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+				.setDescription(`\`${word}\` is more than one word!`);
+
 			Discord.channels.cache
 				.get(CHANNELS.rules)
-				.send(`${msg.author} - \`${word}\` is more than one word!`);
+				.send({
+					content: msg.author.toString(), embeds: [embed],
+					allowedMentions: {users: [msg.author.id]}
+				}
+				);
 			return;
 		}
 
@@ -62,52 +73,105 @@ Discord.on("message", async (msg) => {
 		var response = await fetch({
 			method: "get",
 			url:
-				`https://en.wiktionary.org/w/api.php?action=parse&summary=example&format=json&redirects=true&page=${word}`,
+				`https://en.wiktionary.org/w/api.php?action=parse&summary=example&format=json&redirects=true&page=${word}`, // todo not wikitionary
 		});
 		if (response.data.error) {
 			msg.delete();
+
+			const embed = new MessageEmbed()
+				.setTitle('Not a word!')
+				.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+				.setDescription(`\`${word}\` is not a word!`);
+
 			Discord.channels.cache
 				.get(CHANNELS.rules)
-				.send(`${msg.author} - \`${word}\` is not a word!`);
+				.send({
+					content: msg.author.toString(), embeds: [embed]
+				}
+				);
 			return;
 		}
 
 		// determine if it starts with the last letter of the previous word
-		const lastWord=(await Database.findOne().sort({index:-1}).exec())
-		const shouldStartWith=lastWord?.word.slice(-1)
-		if (shouldStartWith&&shouldStartWith !== word[0]) {
+		const lastWord = (await Database.findOne().sort({index: -1}).exec());
+		const shouldStartWith = lastWord?.word.slice(-1).replace("`","'");
+		if (shouldStartWith && shouldStartWith !== word[0]) {
 			msg.delete();
+
+			const embed = new MessageEmbed()
+				.setTitle('Doesn\'t start with correct character!')
+				.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+				.setDescription(`\`${word}\` does not start with ${shouldStartWith}!`);
+
 			Discord.channels.cache
 				.get(CHANNELS.rules)
-				.send(
-					`${msg.author} - \`${word}\` does not start with ${shouldStartWith}!`,
+				.send({
+					content: msg.author.toString(), embeds: [embed],
+				}
+				);
+			return;
+		}
+
+		if (lastWord?.author===msg.author.id) {
+			msg.delete();
+
+			const embed = new MessageEmbed()
+				.setTitle('Posting twice in a row!')
+				.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+				.setDescription(`No posting twice in a row allowed!`);
+
+			Discord.channels.cache
+				.get(CHANNELS.rules)
+				.send({
+					content: msg.author.toString(), embeds: [embed],
+				}
 				);
 			return;
 		}
 
 		// determine if it has been used before
-		var used = await Database.findOne({word: word}).exec();
+		var used = await Database.findOne({word}).exec();
 		if (used) {
 			msg.delete();
+			const usedMsg = await msg.channel.messages.fetch(used.id);
+			const embed = new MessageEmbed()
+				.setTitle('Duplicate word!')
+				.setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+				.setDescription(`\`${word}\` has [been used before](https://discord.com/channels/${usedMsg.guildId
+					}/${usedMsg.channelId}/${used.id}) by <@${used.author
+					}>!`)
+				.setThumbnail((await Discord.users.fetch(used.author)).displayAvatarURL());
+
 			Discord.channels.cache
 				.get(CHANNELS.rules)
-				.send(
-					`${msg.author} - \`${word}\` has been used before by ${
-						used.author
-					}!\nSee https: //discord.com/channels/${
-						used.guild
-					}/${used.channel}/${used.id}`,
+				.send({
+					content: msg.author.toString(), embeds: [embed],
+				}
 				);
 			return;
 		}
 
 		// all checks out, add to db
-		await new Database({word, author:msg.author.id, id:msg.id, index:(lastWord?.index?? -1)+1}).save();
-	}catch(error){
-		Discord.channels.cache
-			.get(CHANNELS.rules)
-			.send(`Uhoh! I found an error!\n\n\`\`\`js\n${error.message}\`\`\``)
-				}
+		await new Database({word, author: msg.author.id, id: msg.id, index: (lastWord?.index ?? -1) + 1}).save();
+	} catch (error) {
+		try {
+			console.error(error);
+
+			const ruleChannel = Discord.channels.cache
+				.get(CHANNELS.rules);
+			const pingMe=await ruleChannel.guild.members.fetch(ME_ID)
+			const embed = new MessageEmbed()
+				.setTitle('Error!')
+				.setDescription(`Uhoh! I found an error!\n\`\`\`json\n${JSON.stringify(error.replaceAll("```","[3 backticks]"))}\`\`\``); // todo dm me?
+			ruleChannel.send({
+				[pingMe?"content":""]: pingMe?"<@"+ME_ID+">":"",
+				embeds: [embed],
+				allowedMentions: {users: [msg.author.id]}
+			}
+			);
+			return;
+		}catch(errorError){console.error(errorError)}
+	}
 });
 
 Discord.login(process.env.BOT_TOKEN);
