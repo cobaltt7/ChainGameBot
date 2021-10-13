f5ab54cb89afad1a163901e885b46ce1bc16d2bb
RedGuy12 <paul@reid-family.org>
2021-10-13 00:00:32 -0500

    add console log

--- src/games/counting.js
+++ src/games/counting.js
@@ -6,8 +6,9 @@ export default {
 	validWordsOnly: false,
 	twiceInRow: true, //process.env.NODE_ENV !== "production",
 	duplicates: false,
-	manualCheck(number, lastNumber) {
-		if (number === lastNumber + 1) {
+	manualCheck(number, lastNumber = 0) {
+		console.log("---------", number, lastNumber)
+		if (number !== (lastNumber + 1)) {
 			return new MessageEmbed()
 				.setTitle("Incorrect number!")
 				.setDescription(`\`${number}\` does not come after \`${lastNumber}\`!`);
