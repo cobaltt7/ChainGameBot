d015673acab5a2c3f1d43dd9f2d3d7829ebd8a85
RedGuy12 <RedGuy12@users.noreply.github.com>
2021-10-29 14:13:10 +0000

    Lint and format code
    Signed-off-by: GitHub Actions <actions@github.com>

--- src/games/counting.js
+++ src/games/counting.js
@@ -8,7 +8,7 @@ export default {
 	duplicates: false,
 	manualCheck(number, lastNumber = 0) {
 		console.log("---------", number, lastNumber);
-		if (+number !== (lastNumber + 1)) {
+		if (+number !== lastNumber + 1) {
 			return new MessageEmbed()
 				.setTitle("Incorrect number!")
 				.setDescription(`\`${number}\` does not come after \`${lastNumber}\`!`);
