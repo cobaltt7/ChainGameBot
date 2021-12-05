6499a3f528e0afa37e02f703830c11a6d2292946
RedGuy12 <paul@reid-family.org>
2021-12-05 11:01:07 -0600

    add missing `={}`

--- src/games/counting.js
+++ src/games/counting.js
@@ -6,7 +6,7 @@ export default {
 	validWordsOnly: false,
 	twiceInRow: true, //process.env.NODE_ENV !== "production",
 	duplicates: false,
-	manualCheck(number, {word: lastNumber = 0}) {
+	manualCheck(number, {word: lastNumber = 0}={}) {
 		if (+number !== (+lastNumber + 1)) {
 			return new MessageEmbed()
 				.setTitle("Incorrect number!")
