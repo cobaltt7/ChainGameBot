5ab567fd03bc44cbf0a818167e539e9616670be7
RedGuy12 <paul@reid-family.org>
2021-12-06 06:27:45 -0600

    Update counting.js

--- src/games/counting.js
+++ src/games/counting.js
@@ -4,7 +4,7 @@ export default {
 	name: "Counting",
 	match: /^\d+$/,
 	validWordsOnly: false,
-	twiceInRow: true, //process.env.NODE_ENV !== "production",
+	twiceInRow: false, //process.env.NODE_ENV !== "production",
 	duplicates: false,
 	manualCheck(number, last) {
 const lastNumber=last?.word || 0;
